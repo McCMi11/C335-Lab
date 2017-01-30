9a642f8d39fe8e5031a796f32a620eccba9a202c
Michael McCann <mimccann@indiana.edu>
2017-01-30 11:12:57 -0500

    Upload lab 3 Lab 3 is complete modified the led and btn drivers modified the main.c in lab3 to turn lights on and off in circular pattern

--- driver/src/f3d_led.c
+++ driver/src/f3d_led.c
@@ -10,27 +10,47 @@
 
 //intializes the port and pins for the leds on the board
 void f3d_led_init(void) {
- 
+     
+
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_StructInit(&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All; 
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+
 }
 /*Turns on the appropriate led as specified by the parameter.*/
 void f3d_led_on(int led) {
-  /*YOUR CODE HERE*/
+  GPIOE->BSRR = GPIO_Pin_8 << led;
 }
 
 /*Turns off the approiate led as specified by the parameter*/ 
 void f3d_led_off(int led) {
-  /*YOUR CODE HERE*/
+  GPIOE->BRR = GPIO_Pin_8 << led;
 } 
 
 /*Turns on all LEDs*/
 void f3d_led_all_on(void) {
-  /*YOUR CODE HERE*/
+  int x;
+  for(x = 0; x < 8; x++){
+    f3d_led_on(x);
+  }
  
 } 
 
 /*Turns off all LEDs*/
 void f3d_led_all_off(void) {
-  /*YOUR CODE HERE*/
+   int x;
+  for(x = 0; x < 8; x++){
+    f3d_led_off(x);
+  }
 } 
 
 /* led.c ends here */
--- lab3/main.c
+++ lab3/main.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
 #include <stm32f30x.h>  // Pull in include files for F30x standard drivers 
 #include <f3d_led.h>     // Pull in include file for the local drivers
 #include <f3d_user_btn.h>
+//#include <stm32f30x_rcc.o>
+//#include < stm32f30x_gpio.o> 
 
 // Simple looping delay function
 void delay(void) {
@@ -11,7 +15,28 @@ void delay(void) {
 }
 
 int main(void) {
-  while(1);
+  f3d_led_init();
+  f3d_user_btn_init();
+
+
+  while(1){
+    int x;
+    for(x = 0; x < 8; x++){
+      while(user_btn_read()){
+      }
+      f3d_led_on(x);
+      delay();
+    }
+    for(x = 0; x < 8; x++){
+      while(user_btn_read()){
+      }
+      f3d_led_off(x);
+      delay();
+    }
+  
+    //--end first while
+  }
+  //--end main
 }
 
 #ifdef USE_FULL_ASSERT
