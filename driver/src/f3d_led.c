c56c5b6c0f6f84ead28f453492b8395358708d8e
Michael McCann <mimccann@indiana.edu>
2017-01-30 16:24:11 -0500

    Update Drivers Added comments to the drivers of the led and user button.

--- driver/src/f3d_led.c
+++ driver/src/f3d_led.c
@@ -19,49 +19,89 @@
 #include <stm32f30x_rcc.h>
 #include <f3d_led.h>
 
-
-//intializes the port and pins for the leds on the board
+/*****************
+ *   f3d_led_init
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: Initiates all the leds for future use
+ *   Description: Will run scripts to initate all the leds for use
+ *
+ *
+ */
 void f3d_led_init(void) {
-     
-
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);
-
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOE, ENABLE);// starts the clock for the leds
   GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All; 
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-
-  GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-
+  GPIO_StructInit(&GPIO_InitStructure);// sets the structure to initiated
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All; //  initializes the pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;// sets the mode to output
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;// sets the OType to default
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;// sets the speed, fast speed
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;// sets the led to default values
+  GPIO_Init(GPIOE, &GPIO_InitStructure);// finalizes the initialization
 }
-/*Turns on the appropriate led as specified by the parameter.*/
+
+/*****************
+ *   f3d_led_on
+ *
+ *   Input: int led
+ *   Assumptions of input: 0 <= led < 8
+ *   Guarantees about output: Will turn on desired led
+ *   Description: Turns desired led by its corresponding number
+ *
+ *
+ */
 void f3d_led_on(int led) {
-  GPIOE->BSRR = GPIO_Pin_8 << led;
+  GPIOE->BSRR = GPIO_Pin_8 << led;// bitshift as many times as the led number
 }
 
-/*Turns off the approiate led as specified by the parameter*/ 
+/*****************
+ *   f3d_led_off
+ *
+ *   Input: int led
+ *   Assumptions of input: 0 <= led < 8
+ *   Guarantees about output: Will turn off desired led
+ *   Description: Turns off desired led by its corresponding number.
+ *
+ *
+ */
 void f3d_led_off(int led) {
-  GPIOE->BRR = GPIO_Pin_8 << led;
+  GPIOE->BRR = GPIO_Pin_8 << led;// bitshift the led number to turn off the led
 } 
 
 /*Turns on all LEDs*/
+/*****************
+ *   f3d_led_ass_on
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: Turns all leds on
+ *   Description: Will turn all leds of
+ *
+ *
+ */
 void f3d_led_all_on(void) {
-  int x;
-  for(x = 0; x < 8; x++){
-    f3d_led_on(x);
+  int x;// used as the led number
+  for(x = 0; x < 8; x++){// goes through all the leds
+    f3d_led_on(x);// calls a function to turn on led number x
   }
  
 } 
 
-/*Turns off all LEDs*/
+/*****************
+ *   f3d_led_all_off
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: All leds will be off
+ *   Description: Turns off each led
+ *
+ *
+ */
 void f3d_led_all_off(void) {
-   int x;
-  for(x = 0; x < 8; x++){
-    f3d_led_off(x);
+  int x;// used to store led number
+  for(x = 0; x < 8; x++){// goes through all the leds
+    f3d_led_off(x);// uses the led off function to turn off led number x
   }
 } 
 
--- driver/src/f3d_user_btn.c
+++ driver/src/f3d_user_btn.c
@@ -1,25 +1,53 @@
-/**********************************
- *f3d_user_btn.c 
- *contains the init and read functions for the User Button
- *********************************/
+/******************************************
+ *
+ *
+ *   File: f3d_user_btn.c
+ *   Author: Michael McCann/
+ *   School: Indiana University
+ *   Assignment: Lab 3
+ *   Part of: Driver files
+ *   Description: contains the init and read functions for the User Button
+ *   Date Created: 01/26/2017
+ *   Date Modified: 01/30/2017
+ *   Modified By: Michael McCann
+ *
+ *   Revision Description:  Added comments
+ */
+
 #include <stm32f30x.h>
 #include <stm32f30x_gpio.h>
 #include <stm32f30x_rcc.h>
 
-
-/*Initialization of the UserButton*/
+/*****************
+ *   f3d_user_btn_init
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: Gets the user button ready to be called in other functions
+ *   Description: Initialization of the UserButton
+ *
+ *
+ */
 void f3d_user_btn_init(void){
-  GPIO_InitTypeDef GPIO_InitStructure;     
-  GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_StructInit(&GPIO_InitStructure);//used to initialize the structure
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;// used to initialize the pin
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;// allows for user input
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;// sets the user button state to default
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);// starts the user button clock
+  GPIO_Init(GPIOA, &GPIO_InitStructure);// finializes the initialization  
 }
 
-/*reads the User Button*/
+/*****************
+ *   user_btn_read
+ *
+ *   Input: N/A
+ *   Assumptions of input: N/A
+ *   Guarantees about output: 1 if pressed, otherwise 0
+ *   Description: Reads the user button
+ *
+ *
+ */
 int user_btn_read(void){
- return GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0);
+  return GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0);
 }
