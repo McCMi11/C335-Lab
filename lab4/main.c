e674b6f215eb5b375ca3893e2ef79e77acb4d3e6
Michael McCann <mimccann@indiana.edu>
2017-02-02 19:58:53 -0500

    Lab 4

--- lab4/main.c
+++ lab4/main.c
@@ -44,10 +44,32 @@ int main(void) {
   setvbuf(stdin, NULL, _IONBF, 0);
   setvbuf(stdout, NULL, _IONBF, 0);
   setvbuf(stderr, NULL, _IONBF, 0);
+  int c ;  // used to check current char value
+  int wc = 0; // used to count words
+  int l = 0;  // used to count lines
+  int ch = 0;  // used to count chars
+  int prev = 1;  // used to check what prev is, 1 = non space char, 0 = space char
+  int curr = 1;  // same as prev, but used as current
   
-  while (1){
-    putstring("hello"); 
- }
+  // runs until End of File char is found
+  while((c = getchar()) != 0x1b){
+    switch (c){ // will find firs true and run all until first found break below it
+    case ' ':  // literal space char
+    case '\t':  // literal tab char
+    case '\r':  // literal carriage return char
+    case '\f':  // literal from feed char
+    case '\v': curr = 0;  curr = 0; break; // literal vertical tab char
+    case '\n': curr = 0; l++; curr = 0; break;  // literal newline char
+    default: curr = 1; // any other char
+    }
+    ch++; // increments char
+    if(!curr && prev){  // checks for current to be space char and prev to be non space char
+      wc++; // increments word count
+    }
+    prev = curr;  // sets previous char as current
+  }
+
+  printf("words: %d\nChars: %d\nLines: %d\n", wc, ch, l);
 }
 
 #ifdef USE_FULL_ASSERT
