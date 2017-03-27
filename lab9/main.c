987b23653f5d239d42a43dc1f1bb22eec7541728
Michael McCann <mimccann@indiana.edu>
2017-03-27 17:09:43 -0400

    Update lab 9 Working directions and both pictures being drawn

--- lab9/main.c
+++ lab9/main.c
@@ -1,3 +1,4 @@
+
 /******************************************
  *
  *
@@ -33,6 +34,10 @@
 
 int rgb24_to_rgb16(int, int, int);
 void RGB565(char *, uint16_t *);
+int right = 0b011;
+int left = 0b101;
+int up = 0b110;
+int down = 0b0;
 
 int main(void) { 
   setvbuf(stdin, NULL, _IONBF, 0);
@@ -43,44 +48,47 @@ int main(void) {
   f3d_lcd_init();
   f3d_delay_init();
   f3d_rtc_init();
-  int direction = 0;
+  int dir = 0;
   int img_toggle = 1;
   char *color;
-  int i, j, hexColor, k;
+  int i, j, k;
+  uint16_t hexColor;
   int drawn = 0;
-  char *pixel[3];
-  char *pix;
-  unsigned char *data = header_data;
-  uint16_t image[width*height];
-  f3d_lcd_fillScreen(WHITE);
+  char pixel[3];
+  unsigned char *data = header_data;;
   while (1) {
     if(!drawn){
-      /*
+      f3d_lcd_fillScreen(WHITE);
       if(img_toggle == 0){
-	printf("Image 0\n");
 	for(i = 0; i < width*height; i++){
-	  HEADER_PIXEL(data, pixel);
-	  pix = pixel;
-	  RGB565(pix,&image[i]);
+	  HEADER_PIXEL(data,pixel);
+	  RGB565(pixel, &hexColor);
+	  if(dir == 0) f3d_lcd_drawPixelDir((i%128),(i/height),hexColor, down);
+	  else if(dir == 1) f3d_lcd_drawPixelDir((i%128),(i/height),hexColor, up);
+	  else if(dir == 2) f3d_lcd_drawPixelDir((i%128),(i/height),hexColor, left);
+	  else if(dir == 3) f3d_lcd_drawPixelDir((i%128),(i/height),hexColor, right);
 	}
-	for(i = 0; i < width_1; i++){
-	  for(k = 0; k < height_1; k++){
-	    f3d_lcd_drawPixel(i,k,image[i+160*k]);
-	  }
+      }else
+	if(img_toggle == 1){
+	  for(i = 0; i < width_1; i++){
+	    for(k = 0; k < height_1; k++){
+	      color = header_data_cmap_1[header_data_1[i + k*128]];
+	      hexColor = rgb24_to_rgb16((int) *(color), (int) *(color+1), (int) *(color+2));
+	      if(dir == 0) f3d_lcd_drawPixelDir(i,k,hexColor, down);
+	      else if(dir == 1) f3d_lcd_drawPixelDir(i,k,hexColor, up);
+	      else if(dir == 2) f3d_lcd_drawPixelDir(i,k,hexColor, left);
+	      else if(dir == 3) f3d_lcd_drawPixelDir(i,k,hexColor, right);
+	    }
+	  } 
 	}
-	}else*/ 
-      if(img_toggle == 1){
-	for(i = 0; i < width_1; i++){
-	  for(k = 0; k < height_1; k++){
-	    color = header_data_cmap_1[header_data_1[i + k*128]];
-	    hexColor = rgb24_to_rgb16((int) *(color), (int) *(color+1), (int) *(color+2));
-	    f3d_lcd_drawPixel(i,k,hexColor);
-	  }
-	} 
-      }
       drawn = 1;
     }else{
-      
+      /*
+      delay(1000);
+      drawn = 0;
+      dir++;
+      dir %= 4;
+      */
     }
   }
 }
@@ -92,9 +100,9 @@ int rgb24_to_rgb16(int R, int G, int B) {
   return ((R | G) | B);
 }
 void RGB565(char *pix, uint16_t *image){
-  uint16_t *color;
-  *color = rgb24_to_rgb16(pix[0], pix[1], pix[2]);
-  *image = *color;
+  uint16_t color;
+  color = rgb24_to_rgb16(pix[0], pix[1], pix[2]);
+  *image = color;
 }
 
 
@@ -106,6 +114,3 @@ void assert_failed(uint8_t* file, uint32_t line) {
   while (1);
 }
 #endif
-
-/* main.c ends here */
-
